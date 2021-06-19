6c5001cd3708a53db93d8d4ce0dab33b1d432ec7
Asuka-I <1400cm@gmail.com>
2021-06-19 22:33:34 +0900

    列挙型とセレクトボックス

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order(:id)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create!(post_params)
+    redirect_to posts_path
+  end
+
+  def post_params
+    params.require(:post).permit(:genre, :content)
+  end
+end
